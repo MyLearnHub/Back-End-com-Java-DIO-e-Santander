4f12b2ca4edad520f69582246f2fd0ceabf18c11
Paulo-Alvares <paulo.alvares@sptech.school>
2024-07-14 20:38:29 -0300

    feat: starting repository

---
+++ Santander Bootcamp Java/aulas/exemplo-pacotes/comunicacao/FacebookMessenger.java
@@ -0,0 +1,19 @@
+package comunicacao;
+
+public class FacebookMessenger extends ServicoMensagemInstantanea {
+    @Override
+    public void enviarMensagem() {
+        validarConectadoInternet();
+        System.out.println("Enviando mensagem pelo Facebook");
+    }
+
+    @Override
+    public void receberMensagem() {
+        System.out.println("Recebendo mensagem pelo Facebook");
+    }
+
+    @Override
+    public void salvarHistoricoMensagem() {
+
+    }
+}
